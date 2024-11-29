1d02b737daf38caffa984b142e635ba3f9b92975
henrynguyen <henrylknguyen@gmail.com>
2024-11-29 15:38:49 +1100

    Initial commit ADSK030205      // Everytime we lick the fab we perform loadData and we use View Model observe to extract new info value

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -19,8 +20,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         binding.toolbarLayout.title = getString(R.string.app_name)
 
-        val viewModel by viewModels<MainViewModel>()
-        viewModel.loadData()
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -34,17 +34,29 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        })
 
-        // Short implementation of the click listener
+
+        val viewModel by viewModels<MainViewModel>()
+        // We use viewModel.info.observe to display new value
+        viewModel.info.observe(this) {
+            displaySnackBar(it)
+        }
+
+        // Everytime we lick the fab we perform loadData ie
+        // increment private info value
         binding.fab.setOnClickListener { view ->
-            Snackbar.make(
-                view!!,
-                "Replace with your own action",
-                Snackbar.LENGTH_LONG
-            ).show()
+            viewModel.loadData()
         }
 
 //        binding.fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).show()
 //        }
+    }
+
+    private fun displaySnackBar(count: Int) {
+        Snackbar.make(
+            binding.root,
+            "Current value: $count",
+            Snackbar.LENGTH_LONG
+        ).show()
     }
 }
