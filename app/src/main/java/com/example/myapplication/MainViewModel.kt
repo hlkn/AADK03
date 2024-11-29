9f1f0c52331e2a50eaf634c30d133857f39d81ea
henrynguyen <henrylknguyen@gmail.com>
2024-11-29 15:32:05 +1100

    Initial commit ADSK030205 - Create public info to be displayed in the main activity

--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -1,12 +1,14 @@
 package com.example.myapplication
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     val TAG = "MainViewModel"
     private val _info : MutableLiveData<Int> = MutableLiveData()
+    val info : LiveData<Int> = _info
     init {
         Log.i(TAG, "MainViewModel created!")
         _info.value = 0
