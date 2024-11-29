c5e45e8299c77472eb5cf58cf3343db6e6cbb5ec
henrynguyen <henrylknguyen@gmail.com>
2024-11-29 16:36:55 +1100

    Initial commit ADSK030206 Setup for the challenge

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,19 +2,39 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val viewModel by viewModels<MainViewModel>()
+        binding.addQuantityButton.setOnClickListener {
+            viewModel.increaseQuantity()
+        }
+        binding.removeQuantityButton.setOnClickListener {
+            viewModel.decreaseQuantity()
+        }
+        binding.checkoutButton.setOnClickListener {
+            viewModel.checkout()
+        }
+
+//        viewModel.quantity.observe(this) { TODO("Not yet implemented") }
+//        viewModel.totalAmount.observe(this) { TODO("Not yet implemented") }
+
     }
 }
