a5c95b8553d1b10f943c5739e420be42e40b3d81
henrynguyen <henrylknguyen@gmail.com>
2024-11-30 00:21:16 +1100

    Initial commit ADSK030301 Add a new Launcher EntryActivity and navigate to the MainActivity using Intent filter

---
+++ app/src/main/java/com/example/myapplication/EntryActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityEntryBinding
+
+class EntryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEntryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityEntryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.entryButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
