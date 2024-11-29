65dd885f1ea61cae9d1284817b640aa92f143b8f
henrynguyen <henrylknguyen@gmail.com>
2024-11-29 15:14:36 +1100

    Initial commit ADSK030202 Implement Mutable data to keep track in the view model

--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.myapplication
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     val TAG = "MainViewModel"
+    private val _info : MutableLiveData<Int> = MutableLiveData()
     init {
         Log.i(TAG, "MainViewModel created!")
+        _info.value = 0
     }
 
     fun loadData() {
         // Do some work here
-        Log.i(TAG, "lodData() executing...")
+        _info.value = _info.value?.plus(1)
+        //_info.value = _info.value!! + 1
+        Log.i(TAG, "lodData() executing...value = ${_info.value}")
     }
 
 }
